55d36a4c0537d68ff576cfb8eaf1d288c099f966
KwanKon <rhks33rhs@gmail.com>
2023-01-09 21:59:17 +0900

    web, app Login page 배경 설정

---
+++ myapp/src/pages/Login_page.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import '../css/Login_page_background.css'
+
+function Login_page(){
+
+  return(
+    <div className='Login_page_web'>
+      <div className='test'>hi1123418912893913</div>
+    </div>
+  )
+}
+
+export default Login_page;
