ceaf53bc60721f45871109f5f90ef0a10c8eb8d5
KwanKon <rhks33rhs@gmail.com>
2023-01-09 23:35:38 +0900

    web login page 디자인 제작

--- myapp/src/pages/Login_page.jsx
+++ myapp/src/pages/Login_page.jsx
@@ -1,11 +1,21 @@
 import { useEffect, useState } from 'react';
 import '../css/Login_page_background.css'
+import '../css/Login_page.css'
 
 function Login_page(){
 
   return(
-    <div className='Login_page_web'>
-      <div className='test'>hi1123418912893913</div>
+    <div className='Login_page_web Login_page-containter'>
+      <div className='sub-title'>To-Do-List</div>
+      <div className='title'>DDudo</div>
+      <input type='text' className="ID" placeholder='ID'/>
+      <input type='password' className="Password" placeholder='Password' />
+      <div className='next-button'>〉</div>
+      <div className="Line"></div>
+      <div className='register'>
+        <span>회원가입</span>
+        <span className="register-button">〉</span>
+      </div>
     </div>
   )
 }
