8a7e20dbabf65d6ef3182d3469231acaf9974919
Sungsil <ht0729@gachon.ac.kr>
2023-01-14 21:04:26 +0900

    register_page 생성

--- myapp/src/pages/Register_page.jsx
+++ myapp/src/pages/Register_page.jsx
@@ -1,11 +1,24 @@
 import '../css/Login_page_background.css'
-
+import '../css/Register_page.css'
+import { Link } from 'react-router-dom';
 function Register_page(){
 
   return(
-    <div className='Login_page_web'>
-      
+    <>
+    <div className='Register_page-containter'>
+        <div className='regi_title'>Register</div>
+        <Link to = "/" className='previous_button'>〈</Link>
+        <div className= 'regi_form'>
+          <input type='text' className="regi_ID" placeholder='ID'/>
+          <input type='password' className="regi_password" placeholder='Password' />
+          <input type='text' className="regi_name" placeholder='Name' />
+          <input type='text' className="regi_age" placeholder='Age' />
+          <div className="regi_line"></div>
+          <div className = "regi_btn">Register</div>
+        </div>
     </div>
+    <div className='Login_page_web'></div>
+    </>
   )
 }
 
