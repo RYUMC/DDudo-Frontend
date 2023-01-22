8937d99f5be2f34fcd935eeb629da8be4d18f317
Sungsil <ht0729@gachon.ac.kr>
2023-01-22 19:36:00 +0900

    메인 페이지 병합

---
+++ myapp/src/component/modules/Main_calendar_modules/Main_calendar_modules.jsx
@@ -0,0 +1,22 @@
+import './Main_calendar_modules.css'
+import Calendar from 'react-calendar';
+import React, { useState } from 'react';
+import moment from 'moment/moment';
+export function Main_calendar_modules(){
+
+    const [value, onChange] = useState(new Date());
+  
+    return(
+        <div>
+          <Calendar 
+            formatDay={(locale, date) => date.toLocaleString('en', { day: 'numeric' }) } // 날짜 숫자로만 변환
+            next2Label={null} // 다음 월 이동 2번째 버튼 삭제
+            prev2Label={null} // 이전 월 이동 2번째 버튼 삭제
+            onChange={onChange} 
+            locale="en-GB" // 기본 영문으로 설정
+            value={value} />
+            <div>
+                {moment(value).format("YYYY-MM-DD")} 
+            </div>
+        </div> )
+  }
