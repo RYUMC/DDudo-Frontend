f2140d3dd45f40521310acdd0f7e96fb14e6845b
1109min <1109min@naver.com>
2023-01-29 23:16:59 +0900

    Feat: Register function

--- myapp/src/component/modules/Register_modules/Register_modules.jsx
+++ myapp/src/component/modules/Register_modules/Register_modules.jsx
@@ -1,19 +1,40 @@
-import './Register_modules.css'
-import { Link } from 'react-router-dom';
+import "./Register_modules.css";
+import { Link } from "react-router-dom";
 
-export function Register_modules(){
-  return(
-    <div className='Register_modules-containter'>
-        <div className='regi_title'>Register</div>
-        <Link to = "/DDudo-Frontend" className='previous_button'>〈</Link>
-        <div className= 'regi_form'>
-          <input type='text' placeholder='ID'/>
-          <input type='password' placeholder='Password' />
-          <input type='text' placeholder='Name' />
-          <input type='text' placeholder='Age' />
+//
+import React from "react";
+import { RecoilRoot } from "recoil";
+//
+
+export function Register_modules() {
+  const [inputs, setInputs] = React.useState(0);
+
+  const onChange = (event) => {
+    setInputs(event.target.value);
+  };
+
+  return (
+    <RecoilRoot>
+      <div className="Register_modules-containter">
+        <div className="regi_title">Register</div>
+        <Link to="/DDudo-Frontend" className="previous_button">
+          〈
+        </Link>
+        <div className="regi_form">
+          <input
+            value={inputs}
+            id="id"
+            type="text"
+            placeholder="ID"
+            onChange={onChange}
+          />
+          <input type="password" placeholder="Password" />
+          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Age" />
           <div className="regi_line"></div>
-          <div className = "regi_btn">Register</div>
+          <div className="regi_btn">Register</div>
         </div>
-    </div>
-  )
+      </div>
+    </RecoilRoot>
+  );
 }
